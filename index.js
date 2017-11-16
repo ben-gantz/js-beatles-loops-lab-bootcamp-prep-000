4837f480726f83ca298e0a22cca1c5825de8ada3
Ben Gantz <bengantz@gmail.com>
2017-11-16 04:05:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-var instruments = ["guitar", "bass", "vocals", "drums"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 const facts = [
   "He was the last Beatle to learn to drive",
