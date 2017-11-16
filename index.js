4f13ec883645392c2730a76bb8b30a956e901a88
Ben Gantz <bengantz@gmail.com>
2017-11-16 04:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["guitar", "bass", "vocals", "drums"]
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function theBeatlesPlay(musicians, instruments) {
-  Object.assign()
+  var newArray = []
+  for(let i = 0; i < 4; i++) {
+    newArray.push(musicians[i] + "plays" + instruments[i])
+    }
 }
 
 function johnLennonFacts(facts) {
   
 }
 
-function iLoveTheBeatles() {
+function iLoveTheBeatles(number) {
   
 }
